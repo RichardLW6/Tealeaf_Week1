871b26f1bee911c420039b69e5f98363b71c2774
Richard Wilkerson <richard@time2track.com>
2015-01-31 10:57:01 -0500

    Adding Quiz 1, Lesson 1 answers and folder

---
+++ Quiz_Lesson_1/quiz1_myanswers.rb
@@ -0,0 +1,61 @@
+# QUESTION 1
+
+# a = 1
+
+# QUESTION 2
+
+# 1.  Arrays are for ordered lists - they are better for ordering things
+# 2.  Hashes have keys and values, they are better for assigning values
+# 3.  There are different methods to iterate over and work with Arrays vs. Hashes
+# 4.  Arrays are surrounded by [], while Hashes use {}
+
+
+# QUESTION 3
+
+# arr = [1, 2, 3, 3] ====> will return [1, 2, 3, 3]
+
+# [1, 2, 3, 3].uniq ====> will return [1, 2, 3]
+
+# [1, 2, 3, 3].uniq! ====> will return [1, 2, 3]
+
+
+# QUESTION 3
+
+# MAP will iterate over every element in the Array with the block of code given and return that array.
+
+# SELECT will only select certain elements in an Array that meet the criteria of a block of code, then return an array of those elements
+
+
+# QUESTION 4
+
+# hello = {
+#   "1" => 1,
+#   "2" => 2
+# }
+
+# p hello["1"]
+
+
+# QUESTION 5
+
+# "no way!" is returned
+
+
+# QUESTION 6
+
+# x = 4
+
+
+# QUESTION 7
+
+# x is undefined
+
+  3.times do
+    x += 1
+  end
+
+  puts x
+
+
+
+
